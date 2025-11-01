c3ecedf03758544b35b2e53f5b8d6300638e7723
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-10-31 23:51:36 -0400

    feat: update name of path

---
+++ src/modules/votes/votes.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { deleteVoteController, getVoteController, getVoteIdController, patchVotePartialController, postVoteController, putVoteController } from "./votes.controller";
+
+const voteRouter = Router();
+
+//C.R.U.D.
+
+//create
+voteRouter.post('/', postVoteController);
+
+//read
+voteRouter.get('/', getVoteController);
+voteRouter.get('/:id', getVoteIdController);
+
+//update
+voteRouter.put('/:id', putVoteController);
+voteRouter.patch('/:id', patchVotePartialController);
+
+//delete
+voteRouter.delete('/:id', deleteVoteController);
+
+export default voteRouter;
