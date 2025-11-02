de850f29cb978a972a9801ee99fcd6f3ef00a2af
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-11-02 12:29:01 -0400

    feat: add sequelize and function init

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,29 @@ import express from "express";
 import bodyParser from "body-parser";
 import router from "./config/server.routes";
 import { ENV } from "./config/env.config";
+import sequelize from "./config/database.config";
+import "./modules/votes/models/vote.model";
 
-const PORT = ENV.PORT || 3001;
+async function init() {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync({ force: true });
+        console.info("Database connection has been established successfully.");
+        
+        const PORT = ENV.PORT || 3001;
+        const app = express();
 
-const app = express();
+        app.use(bodyParser.json());
+        app.use("/", router);
 
-app.use(bodyParser.json());
-app.use("/", router);
+        app.listen(PORT, () => {
+            console.log(`Server running on http://localhost:${PORT}`)
+        })
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+}
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
-})
+init();
+
+
