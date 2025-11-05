78f2d1307cdd168dda68a66ece010fa98380021b
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-11-05 12:47:20 -0400

    feat: add routes if auth and users

--- src/config/server.routes.ts
+++ src/config/server.routes.ts
@@ -1,13 +1,16 @@
 import { Router, Request, Response } from "express";   
 import healthRouter from "../modules/healthCheck/healthCheck.routes";
 import voteRouter from "../modules/votes/votes.routes";
+import AuthRouter from "../modules/auth/auth.routes";
+import UserRouter from "../modules/users/users.routes";
 
 const router = Router();
 
 router.use("/", healthRouter);
 router.use("/vote", voteRouter);
+router.use("/auth", AuthRouter);
+router.use("/users", UserRouter);
 router.use((req: Request, res: Response) => {
     res.status(404).send({message: "Not Found", status: 404, ok: false});
 });
-
 export default router;
