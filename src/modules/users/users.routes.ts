40733d8c4bd31eca46159c1bff5ff7bfe93c0dfa
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-11-05 23:06:07 -0400

    feat: add routes for users and interface

---
+++ src/modules/users/users.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import { 
+  getUsers, 
+  updateUserPartial, 
+  getUserById, 
+  deleteUser, 
+  getUserProfile,
+} from './users.controller';
+import { validateSesionUser } from '../../middleware/userSesion.middleware';
+import { userRoleValidation } from '../../middleware/userRole.middleware';
+import { UserRole } from './interfaces/users.interface';
+
+const UserRouter = Router();
+
+UserRouter.get('/profile', validateSesionUser, getUserProfile);
+UserRouter.get('/', getUsers); //para admin
+UserRouter.get(
+  '/:id',
+  validateSesionUser,
+  userRoleValidation(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  getUserById,
+);
+UserRouter.patch('/:id', updateUserPartial);
+UserRouter.delete('/:id', deleteUser);
+
+export default UserRouter;
